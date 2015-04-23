d0225d5b6dac9e3a311007cbb9f7ac3bff05acf6
matiasbircz <matiasbircz@hotmail.com>
2015-04-23 12:21:48 -0300

    Roles modelados

---
+++ TP/data/script_creación_inicial.sql
@@ -0,0 +1,43 @@
+USE GD1C2015
+GO
+
+SET LANGUAGE Spanish
+
+/*	****************************************	CREACION DEL SCHENMA	*********************************************** */
+
+IF NOT EXISTS (SELECT 1 FROM [sys].[schemas] WHERE [name] = 'GEM4')
+	EXECUTE ('CREATE SCHEMA GEM4 AUTHORIZATION gd;');
+GO
+
+/*	****************************************	BORRADO DE OBJETOS	*************************************************** */
+
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'GEM4' AND TABLE_NAME = 'Rol_Por_Funcionalidad')
+	DROP TABLE GEM4.Rol_Por_Funcionalidad;
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'GEM4' AND  TABLE_NAME = 'Rol')
+	DROP TABLE GEM4.Rol;
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'GEM4' AND  TABLE_NAME = 'Funcionalidad')
+	DROP TABLE GEM4.Funcionalidad;
+	
+/*	****************************************	CREACION DE LAS TABLAS	*********************************************** */
+CREATE TABLE GEM4.Rol(
+	Rol_Cod 					INT IDENTITY(1,1),
+	Rol_Nombre					NVARCHAR(50) NOT NULL,					
+	Rol_Habilitado				BIT NOT NULL DEFAULT 1,
+	PRIMARY KEY (Rol_Cod)
+	);
+	
+CREATE TABLE GEM4.Funcionalidad(
+	Funcionalidad_Cod			TINYINT ,
+	Funcionalidad_Descripcion	NVARCHAR(50) NOT NULL,
+	Funcionalidad_Habilitada	BIT DEFAULT 1 NOT NULL,
+	PRIMARY KEY (Funcionalidad_Cod)
+	);
+
+CREATE TABLE GEM4.Rol_Por_Funcionalidad(
+	Rol_Cod								INT,
+	Funcionalidad_Cod					TINYINT,
+	Rol_Por_Funcionalidad_Habilitado	BIT DEFAULT 1,	
+	PRIMARY KEY (Rol_Cod, Funcionalidad_Cod),
+	FOREIGN KEY (Rol_Cod) REFERENCES GEM4.Rol(Rol_Cod),
+	FOREIGN KEY (Funcionalidad_Cod) REFERENCES GEM4.Funcionalidad(Funcionalidad_Cod)
+	);

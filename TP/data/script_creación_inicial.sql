3c80eaf43e7be2b2676e71c03544d68f7cf1df06
matiasbircz <matiasbircz@hotmail.com>
2015-04-23 22:59:10 -0300

    Agregadas las funcionalidades y las funcionalidades por rol
    Si encuentran alguna que este mal o que sea para corregir haganlo

--- TP/data/script_creación_inicial.sql
+++ TP/data/script_creación_inicial.sql
@@ -57,6 +57,35 @@ INSERT INTO GEM4.Rol (Rol_Cod,Rol_Nombre) VALUES
 	(2,'Cliente');
 SET IDENTITY_INSERT GEM4.Rol OFF;
 
+INSERT INTO GEM4.Funcionalidad(Funcionalidad_Cod,Funcionalidad_Descripcion) VALUES 
+	(1,'ABM de Rol'),
+	(2,'ABM de Usuario'),
+	(3,'ABM de Clientes'),
+	(4,'ABM de Cuenta'),
+	(5,'Asociar/Desasociar Tarjetas de Credito'),
+	(6,'Depositos'),
+	(7,'Retiros'),
+	(8,'Transferencias'),
+	(9,'Facturacion'),
+	(10,'Consulta Saldos'),
+	(11,'Listados Estadisticos');
+	
+INSERT INTO GEM4.Rol_Por_Funcionalidad (Rol_Cod, Funcionalidad_Cod) VALUES
+	(1,1),
+	(1,2),
+	(1,3),
+	(1,4),
+	(2,4),
+	(2,5),
+	(2,6),
+	(2,7),
+	(2,8),
+	(1,9),
+	(2,9),
+	(1,10),
+	(2,10),
+	(1,11);
+	
 /* ***************************************** STORED PROCEDURES ************************************************** */
 
 

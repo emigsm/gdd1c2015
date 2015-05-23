d3eef649f92450b81b4107829e009f2cafe9e5ae
matiasbircz <matiasbircz@hotmail.com>
2015-05-23 17:01:01 -0300

    Ventana de Consulta de Saldos hecha

---
+++ TP/src/PagoElectronico/Operaciones/Consulta Saldos/ConsultaSaldosPrincipal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PagoElectronico.Operaciones.Consulta_Saldos
+{
+    public partial class ConsultaSaldosPrincipal : Form
+    {
+        public ConsultaSaldosPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Owner.Show();
+            this.Hide();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConsultaSaldosPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

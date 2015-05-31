1a501d87b65604d45b96ab326a324ffdfef3591c
Celiberti,F. Giuliano <giuliano_celiberti@hotmail.com>
2015-05-31 12:37:28 -0300

    Ventana Retirosss

--- TP/src/PagoElectronico/Operaciones/Retiros/RetirosPrincipal.Designer.cs
+++ TP/src/PagoElectronico/Operaciones/Retiros/RetirosPrincipal.Designer.cs
@@ -34,14 +34,20 @@
             this.ImportetextBox = new System.Windows.Forms.TextBox();
             this.nroDoclabel = new System.Windows.Forms.Label();
             this.tipoDocLabel = new System.Windows.Forms.Label();
-            this.tipoDoccomboBox = new System.Windows.Forms.ComboBox();
+            this.tipoDocComboBox = new System.Windows.Forms.ComboBox();
             this.nroDoctextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumeroChequetextBox = new System.Windows.Forms.TextBox();
+            this.GenerarRetirobutton = new System.Windows.Forms.Button();
+            this.VOLVERbutton = new System.Windows.Forms.Button();
+            this.FechaLeyendalabel = new System.Windows.Forms.Label();
+            this.fechaValorLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // cuentacomboBox
             // 
             this.cuentacomboBox.FormattingEnabled = true;
-            this.cuentacomboBox.Location = new System.Drawing.Point(167, 60);
+            this.cuentacomboBox.Location = new System.Drawing.Point(164, 63);
             this.cuentacomboBox.Name = "cuentacomboBox";
             this.cuentacomboBox.Size = new System.Drawing.Size(201, 21);
             this.cuentacomboBox.TabIndex = 0;
@@ -58,7 +64,7 @@
             // importelabel
             // 
             this.importelabel.AutoSize = true;
-            this.importelabel.Location = new System.Drawing.Point(15, 105);
+            this.importelabel.Location = new System.Drawing.Point(12, 108);
             this.importelabel.Name = "importelabel";
             this.importelabel.Size = new System.Drawing.Size(131, 13);
             this.importelabel.TabIndex = 2;
@@ -66,11 +72,10 @@
             // 
             // ImportetextBox
             // 
-            this.ImportetextBox.Location = new System.Drawing.Point(167, 102);
+            this.ImportetextBox.Location = new System.Drawing.Point(164, 105);
             this.ImportetextBox.Name = "ImportetextBox";
             this.ImportetextBox.Size = new System.Drawing.Size(201, 20);
             this.ImportetextBox.TabIndex = 3;
-            this.ImportetextBox.TextChanged += new System.EventHandler(this.ImportetextBox_TextChanged);
             // 
             // nroDoclabel
             // 
@@ -90,13 +95,13 @@
             this.tipoDocLabel.TabIndex = 5;
             this.tipoDocLabel.Text = "Tipo Documento:";
             // 
-            // tipoDoccomboBox
+            // tipoDocComboBox
             // 
-            this.tipoDoccomboBox.FormattingEnabled = true;
-            this.tipoDoccomboBox.Location = new System.Drawing.Point(108, 149);
-            this.tipoDoccomboBox.Name = "tipoDoccomboBox";
-            this.tipoDoccomboBox.Size = new System.Drawing.Size(121, 21);
-            this.tipoDoccomboBox.TabIndex = 6;
+            this.tipoDocComboBox.FormattingEnabled = true;
+            this.tipoDocComboBox.Location = new System.Drawing.Point(108, 149);
+            this.tipoDocComboBox.Name = "tipoDocComboBox";
+            this.tipoDocComboBox.Size = new System.Drawing.Size(121, 21);
+            this.tipoDocComboBox.TabIndex = 6;
             // 
             // nroDoctextBox
             // 
@@ -105,13 +110,71 @@
             this.nroDoctextBox.Size = new System.Drawing.Size(136, 20);
             this.nroDoctextBox.TabIndex = 7;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Numero de Cheque Librado:";
+            // 
+            // NumeroChequetextBox
+            // 
+            this.NumeroChequetextBox.Location = new System.Drawing.Point(164, 196);
+            this.NumeroChequetextBox.Name = "NumeroChequetextBox";
+            this.NumeroChequetextBox.Size = new System.Drawing.Size(170, 20);
+            this.NumeroChequetextBox.TabIndex = 9;
+            // 
+            // GenerarRetirobutton
+            // 
+            this.GenerarRetirobutton.Location = new System.Drawing.Point(31, 280);
+            this.GenerarRetirobutton.Name = "GenerarRetirobutton";
+            this.GenerarRetirobutton.Size = new System.Drawing.Size(184, 43);
+            this.GenerarRetirobutton.TabIndex = 10;
+            this.GenerarRetirobutton.Text = "Generar Retiro";
+            this.GenerarRetirobutton.UseVisualStyleBackColor = true;
+            // 
+            // VOLVERbutton
+            // 
+            this.VOLVERbutton.Location = new System.Drawing.Point(238, 280);
+            this.VOLVERbutton.Name = "VOLVERbutton";
+            this.VOLVERbutton.Size = new System.Drawing.Size(193, 43);
+            this.VOLVERbutton.TabIndex = 11;
+            this.VOLVERbutton.Text = "Volver";
+            this.VOLVERbutton.UseVisualStyleBackColor = true;
+            // 
+            // FechaLeyendalabel
+            // 
+            this.FechaLeyendalabel.AutoSize = true;
+            this.FechaLeyendalabel.Location = new System.Drawing.Point(21, 248);
+            this.FechaLeyendalabel.Name = "FechaLeyendalabel";
+            this.FechaLeyendalabel.Size = new System.Drawing.Size(43, 13);
+            this.FechaLeyendalabel.TabIndex = 12;
+            this.FechaLeyendalabel.Text = "Fecha :";
+            // 
+            // fechaValorLabel
+            // 
+            this.fechaValorLabel.AutoSize = true;
+            this.fechaValorLabel.Location = new System.Drawing.Point(167, 248);
+            this.fechaValorLabel.Name = "fechaValorLabel";
+            this.fechaValorLabel.Size = new System.Drawing.Size(64, 13);
+            this.fechaValorLabel.TabIndex = 13;
+            this.fechaValorLabel.Text = "Valor Fecha";
+            // 
             // RetirosPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(480, 335);
+            this.Controls.Add(this.fechaValorLabel);
+            this.Controls.Add(this.FechaLeyendalabel);
+            this.Controls.Add(this.VOLVERbutton);
+            this.Controls.Add(this.GenerarRetirobutton);
+            this.Controls.Add(this.NumeroChequetextBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.nroDoctextBox);
-            this.Controls.Add(this.tipoDoccomboBox);
+            this.Controls.Add(this.tipoDocComboBox);
             this.Controls.Add(this.tipoDocLabel);
             this.Controls.Add(this.nroDoclabel);
             this.Controls.Add(this.ImportetextBox);
@@ -120,7 +183,6 @@
             this.Controls.Add(this.cuentacomboBox);
             this.Name = "RetirosPrincipal";
             this.Text = "RetirosPrincipal";
-            this.Load += new System.EventHandler(this.RetirosPrincipal_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,7 +196,13 @@
         private System.Windows.Forms.TextBox ImportetextBox;
         private System.Windows.Forms.Label nroDoclabel;
         private System.Windows.Forms.Label tipoDocLabel;
-        private System.Windows.Forms.ComboBox tipoDoccomboBox;
+        private System.Windows.Forms.ComboBox tipoDocComboBox;
         private System.Windows.Forms.TextBox nroDoctextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NumeroChequetextBox;
+        private System.Windows.Forms.Button GenerarRetirobutton;
+        private System.Windows.Forms.Button VOLVERbutton;
+        private System.Windows.Forms.Label FechaLeyendalabel;
+        private System.Windows.Forms.Label fechaValorLabel;
     }
 }
--- TP/src/PagoElectronico/Operaciones/Retiros/RetirosPrincipal.cs
+++ TP/src/PagoElectronico/Operaciones/Retiros/RetirosPrincipal.cs
@@ -6,14 +6,32 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using PagoElectronico.Utilidades.ModeloSistema;
 
 namespace PagoElectronico.Operaciones.Retiros
 {
     public partial class RetirosPrincipal : Form
     {
-        public RetirosPrincipal()
+        public RetirosPrincipal(string username)
         {
             InitializeComponent();
+
+            InitializeComponent();
+            string usuario = username;
+            DataTable cuenta = GestorDeSistema.obtenerCuentasDeUsuario(usuario);
+            cuentacomboBox.DisplayMember = "Cuenta_Numero";
+            cuentacomboBox.ValueMember = "Cuenta_Numero";
+            cuentacomboBox.DataSource = cuenta;
+
+            DataTable tiposDoc = GestorDeSistema.obtenerTiposDoc();
+            tipoDocComboBox.DisplayMember = "Documento_Tipo_Descripcion";
+            tipoDocComboBox.ValueMember = "Documento_Tipo_Codigo";
+            tipoDocComboBox.DataSource = tiposDoc;
+
+       
+            fechaValorLabel.Text = Convert.ToString(GestorDeSistema.solicitarFecha());
+
+
         }
     }
 }

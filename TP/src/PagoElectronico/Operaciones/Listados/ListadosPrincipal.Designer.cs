9a33fa30320d80d6bd673d93926cfe0fdfc593cf
matiasbircz <matiasbircz@hotmail.com>
2015-06-05 18:26:14 -0300

    Ventana de Estadisticas Hecha

--- TP/src/PagoElectronico/Operaciones/Listados/ListadosPrincipal.Designer.cs
+++ TP/src/PagoElectronico/Operaciones/Listados/ListadosPrincipal.Designer.cs
@@ -28,11 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.GBListados = new System.Windows.Forms.GroupBox();
+            this.cmbTrimestre = new System.Windows.Forms.ComboBox();
+            this.lblTrimestre = new System.Windows.Forms.Label();
+            this.cmbAnio = new System.Windows.Forms.ComboBox();
+            this.lblAño = new System.Windows.Forms.Label();
+            this.cmbListados = new System.Windows.Forms.ComboBox();
+            this.lblSeleccionListado = new System.Windows.Forms.Label();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.GBListados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GBListados
+            // 
+            this.GBListados.Controls.Add(this.cmbTrimestre);
+            this.GBListados.Controls.Add(this.lblTrimestre);
+            this.GBListados.Controls.Add(this.cmbAnio);
+            this.GBListados.Controls.Add(this.lblAño);
+            this.GBListados.Controls.Add(this.cmbListados);
+            this.GBListados.Controls.Add(this.lblSeleccionListado);
+            this.GBListados.Location = new System.Drawing.Point(13, 14);
+            this.GBListados.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GBListados.Name = "GBListados";
+            this.GBListados.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GBListados.Size = new System.Drawing.Size(637, 140);
+            this.GBListados.TabIndex = 1;
+            this.GBListados.TabStop = false;
+            this.GBListados.Text = "Listado Estadistico";
+            // 
+            // cmbTrimestre
+            // 
+            this.cmbTrimestre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTrimestre.FormattingEnabled = true;
+            this.cmbTrimestre.Location = new System.Drawing.Point(310, 29);
+            this.cmbTrimestre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbTrimestre.Name = "cmbTrimestre";
+            this.cmbTrimestre.Size = new System.Drawing.Size(121, 28);
+            this.cmbTrimestre.TabIndex = 9;
+            // 
+            // lblTrimestre
+            // 
+            this.lblTrimestre.AutoSize = true;
+            this.lblTrimestre.Location = new System.Drawing.Point(220, 34);
+            this.lblTrimestre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTrimestre.Name = "lblTrimestre";
+            this.lblTrimestre.Size = new System.Drawing.Size(79, 20);
+            this.lblTrimestre.TabIndex = 8;
+            this.lblTrimestre.Text = "Trimestre:";
+            // 
+            // cmbAnio
+            // 
+            this.cmbAnio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAnio.FormattingEnabled = true;
+            this.cmbAnio.Location = new System.Drawing.Point(62, 29);
+            this.cmbAnio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbAnio.Name = "cmbAnio";
+            this.cmbAnio.Size = new System.Drawing.Size(148, 28);
+            this.cmbAnio.TabIndex = 7;
+            // 
+            // lblAño
+            // 
+            this.lblAño.AutoSize = true;
+            this.lblAño.Location = new System.Drawing.Point(9, 34);
+            this.lblAño.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAño.Name = "lblAño";
+            this.lblAño.Size = new System.Drawing.Size(42, 20);
+            this.lblAño.TabIndex = 6;
+            this.lblAño.Text = "Año:";
+            // 
+            // cmbListados
+            // 
+            this.cmbListados.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbListados.FormattingEnabled = true;
+            this.cmbListados.Location = new System.Drawing.Point(9, 91);
+            this.cmbListados.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbListados.Name = "cmbListados";
+            this.cmbListados.Size = new System.Drawing.Size(620, 28);
+            this.cmbListados.TabIndex = 5;
+            // 
+            // lblSeleccionListado
+            // 
+            this.lblSeleccionListado.AutoSize = true;
+            this.lblSeleccionListado.Location = new System.Drawing.Point(9, 66);
+            this.lblSeleccionListado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSeleccionListado.Name = "lblSeleccionListado";
+            this.lblSeleccionListado.Size = new System.Drawing.Size(159, 20);
+            this.lblSeleccionListado.TabIndex = 4;
+            this.lblSeleccionListado.Text = "Seleccione el Listado";
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(658, 105);
+            this.btnVolver.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(112, 35);
+            this.btnVolver.TabIndex = 7;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(658, 61);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(112, 35);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(658, 16);
+            this.btnListar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(112, 35);
+            this.btnListar.TabIndex = 5;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.AllowUserToAddRows = false;
+            this.dgvResultados.AllowUserToDeleteRows = false;
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(13, 164);
+            this.dgvResultados.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.ReadOnly = true;
+            this.dgvResultados.RowTemplate.Height = 28;
+            this.dgvResultados.Size = new System.Drawing.Size(900, 435);
+            this.dgvResultados.TabIndex = 8;
+            // 
+            // ListadosPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 609);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.GBListados);
+            this.Name = "ListadosPrincipal";
             this.Text = "ListadosPrincipal";
+            this.GBListados.ResumeLayout(false);
+            this.GBListados.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox GBListados;
+        private System.Windows.Forms.ComboBox cmbTrimestre;
+        private System.Windows.Forms.Label lblTrimestre;
+        private System.Windows.Forms.ComboBox cmbAnio;
+        private System.Windows.Forms.Label lblAño;
+        private System.Windows.Forms.ComboBox cmbListados;
+        private System.Windows.Forms.Label lblSeleccionListado;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.DataGridView dgvResultados;
+
     }
 }
--- TP/src/PagoElectronico/Operaciones/Listados/ListadosPrincipal.cs
+++ TP/src/PagoElectronico/Operaciones/Listados/ListadosPrincipal.cs
@@ -14,6 +14,45 @@ namespace PagoElectronico.Operaciones.Listados
         public ListadosPrincipal()
         {
             InitializeComponent();
+
+            DataTable dtcmbTrimestre = new DataTable();
+            dtcmbTrimestre.Columns.Add("DisplayMember");
+            dtcmbTrimestre.Columns.Add("ValueMember");
+            dtcmbTrimestre.Rows.Add("Primero", 1);
+            dtcmbTrimestre.Rows.Add("Segundo", 2);
+            dtcmbTrimestre.Rows.Add("Tercero", 3);
+            dtcmbTrimestre.Rows.Add("Cuarto", 4);
+            cmbTrimestre.DataSource = dtcmbTrimestre;
+            cmbTrimestre.DisplayMember = "DisplayMember";
+            cmbTrimestre.ValueMember = "ValueMember";
+
+            DataTable dtcmbAnio = new DataTable();
+            dtcmbAnio.Columns.Add("DisplayMember");
+            dtcmbAnio.Rows.Add("2013");
+            dtcmbAnio.Rows.Add("2014");
+            dtcmbAnio.Rows.Add("2015");
+            dtcmbAnio.Rows.Add("2016");
+            cmbAnio.DataSource = dtcmbAnio;
+            cmbAnio.DisplayMember = "DisplayMember";
+
+            DataTable dtcmbListados = new DataTable();
+            dtcmbListados.Columns.Add("DisplayMember");
+            dtcmbListados.Columns.Add("ValueMember");
+            dtcmbListados.Rows.Add("Clientes con cuentas inhabilitadas por no pagar los costos de transacción", 1);
+            dtcmbListados.Rows.Add("Cliente con mayor cantidad de comisiones facturadas en todas sus cuentas", 2);
+            dtcmbListados.Rows.Add("Clientes con mayor cantidad de transacciones realizadas entre cuentas propias", 3);
+            dtcmbListados.Rows.Add("Países con mayor cantidad de movimientos", 4);
+            dtcmbListados.Rows.Add("Total facturado para los distintos tipos de cuentas", 5);
+            cmbListados.DataSource = dtcmbListados;
+            cmbListados.DisplayMember = "DisplayMember";
+            cmbListados.ValueMember = "ValueMember";
+
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Owner.Show();
+            this.Hide();
         }
     }
 }

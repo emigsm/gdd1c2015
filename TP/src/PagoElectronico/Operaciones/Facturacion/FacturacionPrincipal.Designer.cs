998475f87d8ca88d3992a9ac49e059c872edaebe
emigsm <emigs18@gmail.com>
2015-06-09 12:50:22 -0300

    Empezando diseño facturación

--- TP/src/PagoElectronico/Operaciones/Facturacion/FacturacionPrincipal.Designer.cs
+++ TP/src/PagoElectronico/Operaciones/Facturacion/FacturacionPrincipal.Designer.cs
@@ -28,11 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.opGenerarFactura = new System.Windows.Forms.RadioButton();
+            this.opComprarSuscripcion = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // opGenerarFactura
+            // 
+            this.opGenerarFactura.AutoSize = true;
+            this.opGenerarFactura.Location = new System.Drawing.Point(15, 27);
+            this.opGenerarFactura.Name = "opGenerarFactura";
+            this.opGenerarFactura.Size = new System.Drawing.Size(102, 17);
+            this.opGenerarFactura.TabIndex = 0;
+            this.opGenerarFactura.TabStop = true;
+            this.opGenerarFactura.Text = "Generar Factura";
+            this.opGenerarFactura.UseVisualStyleBackColor = true;
+            // 
+            // opComprarSuscripcion
+            // 
+            this.opComprarSuscripcion.AutoSize = true;
+            this.opComprarSuscripcion.Location = new System.Drawing.Point(176, 27);
+            this.opComprarSuscripcion.Name = "opComprarSuscripcion";
+            this.opComprarSuscripcion.Size = new System.Drawing.Size(185, 17);
+            this.opComprarSuscripcion.TabIndex = 1;
+            this.opComprarSuscripcion.TabStop = true;
+            this.opComprarSuscripcion.Text = "Comprar Suscripciones de Cuenta";
+            this.opComprarSuscripcion.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.opComprarSuscripcion);
+            this.groupBox1.Controls.Add(this.opGenerarFactura);
+            this.groupBox1.Location = new System.Drawing.Point(15, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 58);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seleccione la operación a realizar:";
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Location = new System.Drawing.Point(428, 31);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(108, 25);
+            this.btnSeleccionar.TabIndex = 3;
+            this.btnSeleccionar.Text = "Seleccionar";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnSeleccionar_Click);
+            // 
+            // FacturacionPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 83);
+            this.Controls.Add(this.btnSeleccionar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FacturacionPrincipal";
             this.Text = "FacturacionPrincipal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton opGenerarFactura;
+        private System.Windows.Forms.RadioButton opComprarSuscripcion;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSeleccionar;
     }
 }
---
+++ TP/src/PagoElectronico/Operaciones/Facturacion/GenerarFactura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PagoElectronico.Operaciones.Facturacion
+{
+    public partial class GenerarFactura : Form
+    {
+        public GenerarFactura(string usuario)
+        {
+            InitializeComponent();
+        }
+
+        private void GenerarFactura_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
